fc6da56ea5955e5dfa4acf4cf11d2f3383d29931
kazuaki_work <kazuaki.mitsumori@gmail.com>
2025-06-28 20:59:26 +0900

    first_commit

---
+++ src/main/java/plugin/enemyDown/data/PlayerScore.java
@@ -0,0 +1,18 @@
+package plugin.enemyDown.data;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.entity.Player;
+
+/**
+ * EnemyDown のゲームを実行する際のスコア情報を扱うプロジェクト
+ * プレーヤー名、合計点数、日時などの情報を持つ。
+ */
+@Getter
+@Setter
+public class PlayerScore {
+
+  private String playerName;
+  private int score;
+  private int gameTime;
+}
